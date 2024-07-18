44559b85865f1643b5d36bfc5355a6bb4a5710b1
cyphersnake <cyphersnake@pm.me>
2024-07-18 16:03:42 +0200

    chore: expose some API to pub

--- halo2_proofs/src/circuit/value.rs
+++ halo2_proofs/src/circuit/value.rs
@@ -29,6 +29,10 @@ impl<V> Value<V> {
         Self { inner: None }
     }
 
+    pub fn unwrap(self) -> Option<V> {
+        self.inner
+    }
+
     /// Constructs a known value.
     ///
     /// # Examples
@@ -45,7 +49,7 @@ impl<V> Value<V> {
     /// Obtains the inner value for assigning into the circuit.
     ///
     /// Returns `Error::Synthesis` if this is [`Value::unknown()`].
-    pub(crate) fn assign(self) -> Result<V, Error> {
+    pub fn assign(self) -> Result<V, Error> {
         self.inner.ok_or(Error::Synthesis)
     }
 
